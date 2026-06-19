7c75be5ba48bc590cda71ee6c60e0ef52f2a230c
Chris Repp <cbrepp@mail.com>
2026-06-18 21:12:26 -0400

    Made JavaFXVideo a proper composite type and fixed remaining issues with videos

--- src/main/java/app/controller/javafx/node/BaseJavaFXNode.java
+++ src/main/java/app/controller/javafx/node/BaseJavaFXNode.java
@@ -1,5 +1,6 @@
 package app.controller.javafx.node;
 
+import app.Coordinates;
 import app.controller.BaseController;
 import static app.controller.BaseController.logger;
 import app.node.BaseDecoratedNode;
@@ -23,71 +24,80 @@ public abstract class BaseJavaFXNode extends BaseDecoratedNode {
         super(node, controllerNode, parent, viewName, controller);
     }
     
+    public Coordinates getParentDimensions(Node controllerNode) {
+        Object fxParentNode = this.parent.controllerNode;
+        double parentWidth;
+        double parentHeight;
+        if ((fxParentNode != null) && (fxParentNode instanceof Region parentRegion)) {
+            parentWidth = parentRegion.getPrefWidth();
+            parentHeight = parentRegion.getPrefHeight();
+            logger.log(Level.SEVERE, "Parent region preferred dimensions: {0}x{1}", new Object[]{parentWidth, parentHeight});
+        } else if ((fxParentNode != null) && (fxParentNode instanceof Stage parentStage)) {
+            Scene scene = parentStage.getScene();
+            Pane pane = (Pane) scene.getRoot();
+            parentWidth = pane.getPrefWidth();
+            parentHeight = pane.getPrefHeight();
+        } else {
+            if (fxParentNode == null) {
+                logger.log(Level.SEVERE, "Parent has not been defined");
+            } else {
+                Class<?> parentControlClass = fxParentNode.getClass();
+                logger.log(Level.SEVERE, "Parent does not have a supported class: {0}", parentControlClass.getSimpleName());
+            }
+            return null;
+        }
+        Coordinates parentDimensions = new Coordinates((int) Math.round(parentWidth), (int) Math.round(parentHeight));
+        return parentDimensions;
+    }
+    
     public void scaleNode(Node controllerNode) {
         if ((controllerNode != null) && (this.parent != null)) {
-            Object fxParentNode = this.parent.controllerNode;
-            double parentWidth;
-            double parentHeight;
-            if ((fxParentNode != null) && (fxParentNode instanceof Region parentRegion)) {
-                parentWidth = parentRegion.getPrefWidth();
-                parentHeight = parentRegion.getPrefHeight();
-                logger.log(Level.SEVERE, "Parent region preferred dimensions: {0}x{1}", new Object[]{parentWidth, parentHeight});
-            } else if ((fxParentNode != null) && (fxParentNode instanceof Stage parentStage)) {
-                Scene scene = parentStage.getScene();
-                Pane pane = (Pane) scene.getRoot();
-                parentWidth = pane.getPrefWidth();
-                parentHeight = pane.getPrefHeight();
-            } else {
-                if (fxParentNode == null) {
-                    logger.log(Level.SEVERE, "Parent has not been defined");
-                } else {
-                    Class<?> parentControlClass = fxParentNode.getClass();
-                    logger.log(Level.SEVERE, "Parent does not have a supported class: {0}", parentControlClass.getSimpleName());
-                }
+            Coordinates parentDimensions = this.getParentDimensions(controllerNode);
+            if (parentDimensions == null) {
                 return;
             }
 
             if (controllerNode instanceof Region region) {
                 if (node.scaleX != null) {
-                    logger.log(Level.INFO, "Scaling Region to parent width {0} by {1}", new Object[]{parentWidth, node.scaleX});
-                    double prefWidth = Math.round(parentWidth * node.scaleX);
+                    logger.log(Level.INFO, "Scaling Region to parent width {0} by {1}", new Object[]{parentDimensions.x, node.scaleX});
+                    double prefWidth = Math.round(parentDimensions.x * node.scaleX);
                     region.setPrefWidth(prefWidth);
                     region.setMaxWidth(prefWidth);
                 } else {
-                    region.setMaxWidth(parentWidth);
+                    region.setMaxWidth(parentDimensions.x);
                 }
                 if (node.scaleY != null) {
-                    logger.log(Level.INFO, "Scaling Region to parent height {0} by {1}", new Object[]{parentHeight, node.scaleY});
-                    double prefHeight = Math.round(parentHeight * node.scaleY);
+                    logger.log(Level.INFO, "Scaling Region to parent height {0} by {1}", new Object[]{parentDimensions.y, node.scaleY});
+                    double prefHeight = Math.round(parentDimensions.y * node.scaleY);
                     region.setPrefHeight(prefHeight);
                     region.setMaxHeight(prefHeight);
                 } else {
-                    region.setMaxHeight(parentHeight);
+                    region.setMaxHeight(parentDimensions.y);
                 }
             } else if (controllerNode instanceof Rectangle rectangle) {
                 if (node.scaleX != null) {
-                    logger.log(Level.INFO, "Scaling Rectangle to parent width {0} by {1}", new Object[]{parentWidth, node.scaleX});
-                    double prefWidth = Math.round(parentWidth * node.scaleX);
+                    logger.log(Level.INFO, "Scaling Rectangle to parent width {0} by {1}", new Object[]{parentDimensions.x, node.scaleX});
+                    double prefWidth = Math.round(parentDimensions.x * node.scaleX);
                     rectangle.setWidth(prefWidth);
                 } else {
-                    rectangle.setWidth(parentWidth);
+                    rectangle.setWidth(parentDimensions.x);
                 }
                 if (node.scaleY != null) {
-                    logger.log(Level.INFO, "Scaling Rectangle to parent height {0} by {1}", new Object[]{parentHeight, node.scaleY});
-                    double prefHeight = Math.round(parentHeight * node.scaleY);
+                    logger.log(Level.INFO, "Scaling Rectangle to parent height {0} by {1}", new Object[]{parentDimensions.y, node.scaleY});
+                    double prefHeight = Math.round(parentDimensions.y * node.scaleY);
                     rectangle.setHeight(prefHeight);
                 } else {
-                    rectangle.setHeight(parentHeight);
+                    rectangle.setHeight(parentDimensions.y);
                 }
             } else if (controllerNode instanceof MediaView video) {
                 if (node.scaleX != null) {
-                    logger.log(Level.INFO, "Scaling MediaView to parent width {0} by {1}", new Object[]{parentWidth, node.scaleX});
-                    double prefWidth = Math.round(parentWidth * node.scaleX);
+                    logger.log(Level.INFO, "Scaling MediaView to parent width {0} by {1}", new Object[]{parentDimensions.x, node.scaleX});
+                    double prefWidth = Math.round(parentDimensions.x * node.scaleX);
                     video.setFitWidth(prefWidth);
                 }
                 if (node.scaleY != null) {
-                    logger.log(Level.INFO, "Scaling MediaView to parent height {0} by {1}", new Object[]{parentHeight, node.scaleY});
-                    double prefHeight = Math.round(parentHeight * node.scaleY);
+                    logger.log(Level.INFO, "Scaling MediaView to parent height {0} by {1}", new Object[]{parentDimensions.y, node.scaleY});
+                    double prefHeight = Math.round(parentDimensions.y * node.scaleY);
                     video.setFitHeight(prefHeight);
                 }
             } else {
--- src/main/java/app/controller/javafx/node/JavaFXVideo.java
+++ src/main/java/app/controller/javafx/node/JavaFXVideo.java
@@ -1,36 +1,41 @@
 package app.controller.javafx.node;
 
+import app.Coordinates;
+import app.EventListener;
+import app.HorizontalAlignment;
 import app.Layout;
+import app.RelativeCoordinates;
+import app.VerticalAlignment;
 import app.controller.BaseController;
 import static app.controller.BaseController.logger;
 import app.controller.JavaFXApplication;
+import app.node.BaseCompositeNode;
 import app.node.BaseDecoratedNode;
+import app.node.BaseNode;
 import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.logging.Level;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.Node;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
-import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 /**
  *
  * @author repp
  */
-public class JavaFXVideo extends BaseJavaFXNode {
+public class JavaFXVideo extends BaseJavaFXNode implements BaseCompositeNode, EventListener {
     
+    public Layout buttonLayout;
     public final Layout layout;
+    public MediaPlayer mediaPlayer;
+    public MediaView mediaView;
     
     public JavaFXVideo(app.node.Video node, BaseDecoratedNode parent, String viewName, BaseController controller, Layout layout) {
-        super(node, new StackPane(), parent, viewName, controller);
+        super(node, new Pane(), parent, viewName, controller);
         this.layout = layout;
     }
     
@@ -41,7 +46,7 @@ public class JavaFXVideo extends BaseJavaFXNode {
         // TODO - This control should be some sort of a container so that a button can overlay for "Skip"
         
         app.node.Video node = (app.node.Video) this.node;
-        StackPane controllerNode = (StackPane) this.controllerNode;
+        Pane controllerNode = (Pane) this.controllerNode;
         
         URL resource = getClass().getResource(node.file);
         
@@ -53,23 +58,8 @@ public class JavaFXVideo extends BaseJavaFXNode {
         String source = resource.toExternalForm();
         Media media = new Media(source);
         
-        MediaPlayer mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setAutoPlay(true);
-        
-        // Add our own custom skip button
-        Button skipButton = new Button();
-        skipButton.setAlignment(Pos.CENTER);
-        skipButton.setText("skip");
-        Color halfTransparentWhite = Color.rgb(255, 255, 255, 0.5);
-        BackgroundFill fill = new BackgroundFill(halfTransparentWhite, new CornerRadii(4), Insets.EMPTY);
-        skipButton.setBackground(new Background(fill));
-        StackPane.setAlignment(skipButton, Pos.BOTTOM_RIGHT);
-        skipButton.setOnAction(e -> {
-            logger.log(Level.INFO, "Skip button selected: name={0}", node.name);
-            Duration totalDuration = mediaPlayer.getTotalDuration();
-            mediaPlayer.seek(totalDuration);
-            skipButton.setDisable(true);
-        });
+        this.mediaPlayer = new MediaPlayer(media);
+        this.mediaPlayer.setAutoPlay(true);
 
         String eventName = node.name;
         if (node.eventName != null) {
@@ -78,9 +68,9 @@ public class JavaFXVideo extends BaseJavaFXNode {
         final String finalEventname = eventName;
         
         if (node.loop) {
-            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-            mediaPlayer.setOnEndOfMedia(() -> {
-                mediaPlayer.seek(Duration.ZERO);
+            this.mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+            this.mediaPlayer.setOnEndOfMedia(() -> {
+                this.mediaPlayer.seek(Duration.ZERO);
                 if (node.eventListener != null) {
                     logger.log(Level.INFO, "Video complete: name={0}", node.name);
                     node.eventListener.onEvent(finalEventname, node.name);
@@ -88,29 +78,86 @@ public class JavaFXVideo extends BaseJavaFXNode {
             });
         } else {
             if (node.eventListener != null) {
-                mediaPlayer.setOnEndOfMedia(() -> {
+                this.mediaPlayer.setOnEndOfMedia(() -> {
                     logger.log(Level.INFO, "Video complete: name={0}", node.name);
-                    controllerNode.getChildren().remove(skipButton);
+                    //controllerNode.getChildren().remove(skipButton);
                     node.eventListener.onEvent(finalEventname, node.name);
                 });
             }
         }
         
-        MediaView mediaView = new MediaView();
-        mediaView.setMediaPlayer(mediaPlayer);
-        mediaView.setPreserveRatio(true);
-        mediaView.setSmooth(true);
-        controllerNode.getChildren().add(mediaView);
-        controllerNode.getChildren().add(skipButton); // Add the button second so that it appears on top
+        this.mediaView = new MediaView();
+        this.mediaView.setMediaPlayer(this.mediaPlayer);
+        this.mediaView.setPreserveRatio(true);
+        this.mediaView.setSmooth(true);
+        controllerNode.getChildren().add(this.mediaView);
         
         // Add a listener to resizes because once the media has fully loaded, the dimensions will be known
-        mediaView.layoutBoundsProperty().addListener((obs, oldBounds, newBounds) -> {
+        this.mediaView.layoutBoundsProperty().addListener((obs, oldBounds, newBounds) -> {
+            logger.log(Level.INFO, "New media view bounds for {0}", this.node.name);
             if (newBounds.getWidth() > 1 && newBounds.getHeight() > 1) {
-                // TODO - The scaling isn't working because the StackPane is expanding to all the MediaView to be max size
+                controllerNode.setPrefWidth(newBounds.getWidth());
+                controllerNode.setPrefHeight(newBounds.getHeight());
                 this.scaleNode(controllerNode);
                 JavaFXApplication.positionNode((Pane) this.parent.controllerNode, node, this.layout, controllerNode);
+                BaseDecoratedNode buttonDecoratedNode = ((JavaFXApplication) this.controller).getDecoratedNode(this.viewName, this.node.name + "/skip");
+                if (buttonDecoratedNode != null) {
+                    JavaFXApplication.positionNode((Pane) this.controllerNode, buttonDecoratedNode.node, this.buttonLayout, (Node) buttonDecoratedNode.controllerNode);
+                }
             }
         });
+    }
+    
+    @Override
+    public Map<? extends BaseNode, Layout> getChildren() {
+        Map<BaseNode, Layout> children = new LinkedHashMap();
+        app.node.Button skipButton = new app.node.Button(this.node.name + "/skip");
+        skipButton.backgroundColor = new app.color.Color(app.color.Color.BLACK, 0.5);
+        skipButton.eventListener = this;
+        skipButton.isMultiUse = false;
+        skipButton.text = "skip";
+        skipButton.textColor = app.color.Color.WHITE;
+        this.buttonLayout = new Layout(new RelativeCoordinates(1.0, 1.0), HorizontalAlignment.RIGHT, VerticalAlignment.BOTTOM);
+        children.put(skipButton, this.buttonLayout);
+        return children;
+    }
+
+    @Override
+    public void onEvent(String eventName, Object eventValue) {
+        if (eventName.equals(this.node.name + "/skip")) {
+            logger.log(Level.INFO, "Skip button selected: name={0}", node.name);
+            Duration totalDuration = this.mediaPlayer.getTotalDuration();
+            this.mediaPlayer.seek(totalDuration);
+        } else {
+            logger.log(Level.WARNING, "Unsupported event!");
+        }
+    }
+    
+    @Override
+    public void scaleNode(Node controllerNode) {
+        logger.log(Level.INFO, "New media view bounds for {0}", this.node.name);
+        
+        if ((controllerNode == null) || (this.parent == null)) {
+            logger.log(Level.INFO, "Null nodes, nothing to do");
+            return;
+        }
+        
+        Coordinates parentDimensions = this.getParentDimensions(controllerNode);
+        if (parentDimensions == null) {
+            logger.log(Level.WARNING, "No parent dimensions!");
+            return;
+        }
+
+        if (this.node.scaleX != null) {
+            logger.log(Level.INFO, "Scaling MediaView to parent width {0} by {1}", new Object[]{parentDimensions.x, this.node.scaleX});
+            double prefWidth = Math.round(parentDimensions.x * this.node.scaleX);
+            this.mediaView.setFitWidth(prefWidth);
+        }
+        if (this.node.scaleY != null) {
+            logger.log(Level.INFO, "Scaling MediaView to parent height {0} by {1}", new Object[]{parentDimensions.y, this.node.scaleY});
+            double prefHeight = Math.round(parentDimensions.y * this.node.scaleY);
+            this.mediaView.setFitHeight(prefHeight);
+        }
     }
     
 }
